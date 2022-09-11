f02c4435a7dd6039cd3d17d149e2fffee8d82995
Kamolphon <kamolphon.thon@kmutt.ac.th>
2022-09-11 17:06:28 +0700

    evekmp_

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,24 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  if ( (array1 === null || array1 === undefined) && (array2 === undefined || array2 === null) ) {
+      return undefined     
+  }
+
+  if (array1 === [] && array2 === []) {
+    return []
+  }
+
+  if (array1 === [] || array1 === null || array1 === undefined) {
+    return array2
+  }
+
+  if (array2 === [] || array2 === null || array2 === undefined) {
+    return array1
+  }
+
+  else{
+    return array1.concat(array2)
+  }
 }
 module.exports = concatArray
